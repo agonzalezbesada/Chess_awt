ee614ec4ab0a33e50d8ce1f1cad4887cea740ca6
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-05-07 15:58:13 +0200

    Piezas

--- src/Modelo/Caballo.java
+++ src/Modelo/Caballo.java
@@ -1,4 +1,8 @@
 package Modelo;
 
-public class Caballo {
+public class Caballo extends Pieza implements IPieza {
+    @Override
+    public int[] cambiarPosicion() {
+        return new int[0];
+    }
 }
