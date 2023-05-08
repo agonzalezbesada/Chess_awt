ba55343f27aaca8d6151208a00f99feb4214735c
Dani <daniobey24@gmail.com>
2023-05-08 15:50:08 +0200

    Nueva ventana partida, que añade un placeholder para el temporizador, el botón guardar y salir y un tablero

---
+++ src/Vista/MenuTablero.java
@@ -0,0 +1,55 @@
+package Vista;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class MenuTablero {
+
+    public void generarPartida(){
+        JFrame Partida = new JFrame();
+        Partida.setBounds(0,0,1400,1700);
+        MenuTablero Tablero = new MenuTablero();
+
+
+        JPanel tablero = Tablero.generarTablero();
+        JPanel informacionExtra = new JPanel(new GridLayout(2,1));
+
+        //Aquí iría la funcionalidad de generar tablero
+        JLabel Temporizador = new JLabel("03:00");
+        JButton GuardarYSalir = new JButton("Guardar y salir");
+
+        informacionExtra.add(Temporizador);
+        informacionExtra.add(GuardarYSalir);
+        Partida.add(informacionExtra, BorderLayout.EAST);
+    }
+    public JPanel generarTablero() {
+        JPanel tablero = new JPanel(new GridLayout(9, 9));
+
+        // Agregamos las letras en la fila superior
+        tablero.add(new JLabel(""));
+        for (char c = 'A'; c <= 'H'; c++) {
+            JLabel letra = new JLabel(String.valueOf(c), SwingConstants.CENTER);
+            tablero.add(letra);
+        }
+
+        // Agregamos las casillas del tablero
+        for (int i = 1; i <= 8; i++) {
+            // Agregamos los números en la columna izquierda
+            JLabel numero = new JLabel(String.valueOf(i), SwingConstants.CENTER);
+            tablero.add(numero);
+            for (int j = 0; j < 8; j++) {
+                JPanel casilla = new JPanel();
+                if ((i + j) % 2 == 0) {
+                    casilla.setBackground(Color.WHITE);
+                } else {
+                    casilla.setBackground(Color.BLACK);
+                }
+                casilla.setPreferredSize(new Dimension(100, 100));
+                tablero.add(casilla);
+            }
+        }
+
+        return tablero;
+    }
+}
