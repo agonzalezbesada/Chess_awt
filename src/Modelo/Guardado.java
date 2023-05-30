ac76942c77388700937ba19f41b11797a1ea978b
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-05-30 23:52:00 +0200

    Guardado Inicial

---
+++ src/Modelo/Guardado.java
@@ -0,0 +1,61 @@
+package Modelo;
+
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Guardado {
+
+    public Gson gson = new Gson();
+
+    public void guardarPartida(Partida partida) {
+
+        String jsonPartida = gson.toJson(partida, Partida.class);
+
+        FileWriter escritorFichero;
+
+        try {
+
+            escritorFichero = new FileWriter("src\\PartidaGuardada"+partida.nickName+".json");
+            escritorFichero.write(jsonPartida);
+            escritorFichero.close();
+
+        } catch (IOException e) {
+
+            System.out.println(e.toString());
+
+        }
+
+    }
+
+    public Partida cargarPartida(String jugador) {
+
+        String jsonRecuperado = "";
+        FileReader lectorFichero;
+
+        try {
+
+            lectorFichero = new FileReader("src\\PartidaGuardada"+jugador+".json");
+            int caracter = lectorFichero.read();
+
+            while (caracter!=-1) {
+
+                jsonRecuperado += (char) caracter;
+                caracter = lectorFichero.read();
+
+            }
+
+            lectorFichero.close();
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+
+        Partida partidaCargada = gson.fromJson(jsonRecuperado, Partida.class);
+
+        return partidaCargada;
+    }
+
+}
