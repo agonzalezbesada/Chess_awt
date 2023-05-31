041b700e8a8de7dc0e284a2fdcdfbad3da5d7add
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-05-31 18:09:10 +0200

    Iniciales Variables

--- src/Vista/MenuPrincipal.java
+++ src/Vista/MenuPrincipal.java
@@ -1,5 +1,7 @@
 package Vista;
 
+import Controlador.Controlador;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -16,38 +18,46 @@ public class MenuPrincipal {
      */
     public static void GenerarMenu(){
 
-        JFrame MenuPrincipal = new JFrame(); // Pantalla del menu principal
-        MenuPrincipal.setTitle("Ajedrez"); // Nombre de la pantalla
-        MenuPrincipal.setBounds(0,0, 1920, 1050); // Proporciones
-        MenuPrincipal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JFrame menuPrincipal = new JFrame(); // Pantalla del menu principal
+        menuPrincipal.setTitle("Ajedrez"); // Nombre de la pantalla
+        menuPrincipal.setBounds(0,0, 1920, 1050); // Proporciones
+        menuPrincipal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         // Tras crear el menú principal, generamos un panel donde introducimos un GridLayout con 4 filas y 1 columna
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(4, 1));
 
-        JButton JugarPartida = new JButton("Jugar partida"); // Boton para jugar la partida
-        JButton IniciarSesion = new JButton("Iniciar sesión"); // Boton para iniciar sesion
-        JButton Estadisticas = new JButton("Consultar estadísticas"); // Boton de estadisticas
-        JButton Salir = new JButton("Salir"); // Boton para salir
+        JButton jugarPartida = new JButton("Jugar partida"); // Boton para jugar la partida
+        JButton iniciarSesion = new JButton("Iniciar sesión"); // Boton para iniciar sesion
+        JButton estadisticas = new JButton("Consultar estadísticas"); // Boton de estadisticas
+        JButton salir = new JButton("Salir"); // Boton para salir
 
-        Salir.addActionListener(new ActionListener() {
+        salir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0); // Finaliza el programa
             }
         });
-        JugarPartida.addActionListener(new ActionListener() {
+
+        iniciarSesion.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                MenuPrincipal.setVisible(false); // Cierra la ventana
+                Controlador.iniciarSesion("Adrian");
+            }
+        });
+
+        jugarPartida.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuPrincipal.setVisible(false); // Cierra la ventana
                 MenuTablero.generarPartida(0); // Genera la ventana de la partida
             }
         });
 
         // Añade los botones
-        panel.add(JugarPartida);
-        panel.add(IniciarSesion);
-        panel.add(Estadisticas);
-        panel.add(Salir);
+        panel.add(jugarPartida);
+        panel.add(iniciarSesion);
+        panel.add(estadisticas);
+        panel.add(salir);
 
         /** Añadimos los botones al panel, y le ponemos una especie de padding con unos límites para separar estos del
          * frame **/
@@ -56,9 +66,9 @@ public class MenuPrincipal {
         // Add an EmptyBorder with a 50-pixel margin around the panel
         panel.setBorder(new EmptyBorder(150, 200, 150, 200));
 
-        MenuPrincipal.add(panel, BorderLayout.CENTER);
+        menuPrincipal.add(panel, BorderLayout.CENTER);
 
-        MenuPrincipal.setVisible(true);
+        menuPrincipal.setVisible(true);
     }
 
 }
